7f3e0acc61242ddd50958aa07fd7d31dce23eb78
Jani Rahkola <jani.rahkola@gmail.com>
2012-07-01 23:16:41 +0300

    init

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject pantry "0.0.1-SNAPSHOT"
+  :description ""
+  :url "https://github.com/rapala/pantry"
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ test/pantry/core_test.clj
@@ -0,0 +1,7 @@
+(ns pantry.core-test
+  (:use clojure.test
+        pantry.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
